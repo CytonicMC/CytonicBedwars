1ad0859425756ce49a322136b9ee968a014a77f1
Foxikle <tristantroop260@gmail.com>
2023-12-02 10:09:05 -0500

    Kill handling

--- src/main/java/dev/foxikle/webnetbedwars/listeners/DamageListener.java
+++ src/main/java/dev/foxikle/webnetbedwars/listeners/DamageListener.java
@@ -1,10 +1,12 @@
 package dev.foxikle.webnetbedwars.listeners;
 
 import dev.foxikle.webnetbedwars.WebNetBedWars;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class DamageListener implements Listener {
 
@@ -18,17 +20,23 @@ public class DamageListener implements Listener {
     @EventHandler
     public void onKill(EntityDamageByEntityEvent event) {
         if(event.getEntity() instanceof Player player) {
+
+            if(event.getDamager() instanceof Player damager) {
+                plugin.getGameManager().getStatsManager().addPlayerDamageDealt(damager.getUniqueId(), event.getFinalDamage());
+            }
+            plugin.getGameManager().getStatsManager().addPlayerDamageTaken(player.getUniqueId(), event.getFinalDamage());
+
+
             if(player.getHealth() - event.getFinalDamage() <= 0) {
-                //todo: check for final kills
-                if(event.getDamager() instanceof Player damager){
-                    plugin.getGameManager().getStatsManager().addPlayerKill(damager.getUniqueId());
-                    plugin.getGameManager().getStatsManager().addPlayerDamageDealt(damager.getUniqueId(), event.getFinalDamage());
-                }
-                plugin.getGameManager().getStatsManager().addPlayerDeath(player.getUniqueId());
-                plugin.getGameManager().getStatsManager().addPlayerDamageTaken(player.getUniqueId(), event.getFinalDamage());
-                // dead
-                //todo: animations and stuff
                 event.setCancelled(true);
+                if(event.getDamager() instanceof Player damager){
+                    plugin.getGameManager().kill(player, damager, EntityDamageEvent.DamageCause.KILL);
+                    return;
+                }
+                if(event.getDamager() instanceof Fireball && event.getCause() == EntityDamageEvent.DamageCause.PROJECTILE) {
+                    return;
+                }
+                plugin.getGameManager().kill(player, null, event.getCause());
             }
         }
     }
