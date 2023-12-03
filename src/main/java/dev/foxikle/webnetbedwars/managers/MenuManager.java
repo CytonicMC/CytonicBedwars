02888307ef71c7471b68ba293047dd434619d765
Foxikle <tristantroop260@gmail.com>
2023-12-03 08:38:40 -0500

    Create a menu for Spectator flight speed

--- src/main/java/dev/foxikle/webnetbedwars/managers/MenuManager.java
+++ src/main/java/dev/foxikle/webnetbedwars/managers/MenuManager.java
@@ -6,12 +6,15 @@ import me.flame.menus.items.MenuItem;
 import me.flame.menus.menu.ActionResponse;
 import me.flame.menus.menu.Menu;
 import me.flame.menus.menu.Menus;
+import me.flame.menus.menu.fillers.Filler;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.persistence.PersistentDataType;
 
@@ -28,7 +31,7 @@ public class MenuManager {
     }
 
     public Menu getSpectatorSelectorMenu() {
-        Menu menu = Menus.menu().title("Spectator Menu?").rows(2).addAllModifiers().create();
+        Menu menu = Menus.menu().title("Spectate a Player").rows(2).addAllModifiers().create();
 
         plugin.getGameManager().getAlivePlayers().forEach(uuid -> {
             ItemStack head = new ItemStack(Material.PLAYER_HEAD);
@@ -45,6 +48,79 @@ public class MenuManager {
             });
             menu.addItem(item);
         });
+
+        return menu;
+    }
+
+    public Menu getSpectatorSpeedMenu() {
+        Menu menu = Menus.menu().title("Flight Speed").rows(3).addAllModifiers().create();
+
+        ItemStack tenthSpeed = new ItemStack(Material.LEATHER_BOOTS);
+        ItemMeta tenthMeta = tenthSpeed.getItemMeta();
+        tenthMeta.addItemFlags(ItemFlag.values()); // adds all the flags
+        tenthMeta.setDisplayName(ChatColor.GREEN + "0.1x flight speed");
+        tenthSpeed.setItemMeta(tenthMeta);
+
+        menu.setItem(11, ItemBuilder.of(tenthSpeed).buildItem((slot, event) -> {
+            event.getPlayer().setFlySpeed(0.01f);
+            event.getPlayer().closeInventory();
+            event.getPlayer().sendMessage(ChatColor.GREEN + "Your flight speed is now " + ChatColor.GOLD + "0.1x");
+            return ActionResponse.DONE;
+        }));
+
+        ItemStack halfSpeed = new ItemStack(Material.CHAINMAIL_BOOTS);
+        ItemMeta halfMeta = halfSpeed.getItemMeta();
+        halfMeta.addItemFlags(ItemFlag.values()); // adds all the flags
+        halfMeta.setDisplayName(ChatColor.GREEN + ".5x flight speed");
+        halfSpeed.setItemMeta(halfMeta);
+
+        menu.setItem(12, ItemBuilder.of(halfSpeed).buildItem((slot, event) -> {
+            event.getPlayer().setFlySpeed(0.05f);
+            event.getPlayer().closeInventory();
+            event.getPlayer().sendMessage(ChatColor.GREEN + "Your flight speed is now " + ChatColor.GOLD + "0.5x");
+            return ActionResponse.DONE;
+        }));
+
+        ItemStack normalSpeed = new ItemStack(Material.IRON_BOOTS);
+        ItemMeta normalMeta = normalSpeed.getItemMeta();
+        normalMeta.addItemFlags(ItemFlag.values()); // adds all the flags
+        normalMeta.setDisplayName(ChatColor.GREEN + "1x flight speed");
+        normalSpeed.setItemMeta(normalMeta);
+
+        menu.setItem(13, ItemBuilder.of(normalSpeed).buildItem((slot, event) -> {
+            event.getPlayer().setFlySpeed(0.1f);
+            event.getPlayer().closeInventory();
+            event.getPlayer().sendMessage(ChatColor.GREEN + "Your flight speed is now " + ChatColor.GOLD + "1x");
+            return ActionResponse.DONE;
+        }));
+
+        ItemStack x2Speed = new ItemStack(Material.GOLDEN_BOOTS);
+        ItemMeta x2Meta = x2Speed.getItemMeta();
+        x2Meta.addItemFlags(ItemFlag.values()); // adds all the flags
+        x2Meta.setDisplayName(ChatColor.GREEN + "2x flight speed");
+        x2Speed.setItemMeta(x2Meta);
+
+        menu.setItem(14, ItemBuilder.of(x2Speed).buildItem((slot, event) -> {
+            event.getPlayer().setFlySpeed(0.2f);
+            event.getPlayer().closeInventory();
+            event.getPlayer().sendMessage(ChatColor.GREEN + "Your flight speed is now " + ChatColor.GOLD + "2x");
+            return ActionResponse.DONE;
+        }));
+
+        ItemStack x5Speed = new ItemStack(Material.DIAMOND_BOOTS);
+        ItemMeta x5Meta = x5Speed.getItemMeta();
+        x5Meta.addItemFlags(ItemFlag.values()); // adds all the flags
+        x5Meta.setDisplayName(ChatColor.GREEN + "5x flight speed");
+        x5Speed.setItemMeta(x5Meta);
+
+        menu.setItem(15, ItemBuilder.of(x5Speed).buildItem((slot, event) -> {
+            event.getPlayer().setFlySpeed(0.5f);
+            event.getPlayer().closeInventory();
+            event.getPlayer().sendMessage(ChatColor.GREEN + "Your flight speed is now " + ChatColor.GOLD + "5x");
+            return ActionResponse.DONE;
+        }));
+
+
 
         return menu;
     }
