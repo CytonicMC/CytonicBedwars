4977685e8893c72d4aa49731843aaedd327e129f
Foxikle <tristantroop260@gmail.com>
2023-12-03 13:28:55 -0500

    Change gamemode change to only wipe inv if coming from spectator

--- src/main/java/dev/foxikle/webnetbedwars/listeners/GamemodeChangeListener.java
+++ src/main/java/dev/foxikle/webnetbedwars/listeners/GamemodeChangeListener.java
@@ -48,7 +48,8 @@ public class GamemodeChangeListener implements Listener {
             }, 1);
         } else {
             // un-ivisafy if they are respawning, etc.
-            player.getInventory().clear();
+            if(event.getPlayer().getGameMode() == GameMode.ADVENTURE)
+                player.getInventory().clear(); // only clear the inventory if they are coming from a spectator
             plugin.getGameManager().spectators.remove(player.getUniqueId());
             player.removePotionEffect(PotionEffectType.INVISIBILITY);
             Bukkit.getOnlinePlayers().forEach(p -> {
