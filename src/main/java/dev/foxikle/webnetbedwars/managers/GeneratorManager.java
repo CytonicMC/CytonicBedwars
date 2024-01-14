636762038d93c2a61c38ef861fda7731e1f076e6
Webhead1104 <minecraft1234new@hotmail.com>
2024-01-14 14:40:11 -0600

    Removing extra lines

--- src/main/java/dev/foxikle/webnetbedwars/managers/GeneratorManager.java
+++ src/main/java/dev/foxikle/webnetbedwars/managers/GeneratorManager.java
@@ -4,13 +4,11 @@ import dev.foxikle.webnetbedwars.WebNetBedWars;
 import dev.foxikle.webnetbedwars.data.objects.Team;
 import dev.foxikle.webnetbedwars.utils.Items;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.World;
 
 public class GeneratorManager {
     private final WebNetBedWars plugin;
     private final GameManager gameManager;
-
 
     public GeneratorManager(WebNetBedWars plugin, GameManager gameManager) {
         this.plugin = plugin;
@@ -27,9 +25,5 @@ public class GeneratorManager {
 //gold
             Bukkit.getScheduler().runTaskTimer(plugin, () -> world.dropItemNaturally(t.generatorLocation(), Items.GOLD), 0, plugin.getConfig().getInt("GeneratorsWaitTime.gold"));
         }
-
-
-
     }
 }
-
