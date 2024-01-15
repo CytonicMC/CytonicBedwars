8e1d7d12d57b8e3fcf0f34940755b5ce6a92c82e
Foxikle <tristantroop260@gmail.com>
2024-01-15 16:28:21 -0500

    Update game states

--- src/main/java/dev/foxikle/webnetbedwars/data/enums/GameState.java
+++ src/main/java/dev/foxikle/webnetbedwars/data/enums/GameState.java
@@ -1,13 +1,24 @@
 package dev.foxikle.webnetbedwars.data.enums;
 
 public enum GameState {
-    WAITING, // not yet started
-    PLAY,
-    DEATHMATCH,
-    ENDED,
-    CLEANUP,
+    WAITING(-1), // not yet started
+    PLAY(900), // 15 minutes
+    DEATHMATCH(300), // 5 minutes
+    SUDDEN_DEATH(180), // withers - 3 minutes
+    ENDED(30), // 30 sec
+    CLEANUP(-1),
 
     // admin things
-    FROZEN
+    FROZEN(-1);
 
+    // in seconds
+    private final int duration;
+
+    GameState(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 }
