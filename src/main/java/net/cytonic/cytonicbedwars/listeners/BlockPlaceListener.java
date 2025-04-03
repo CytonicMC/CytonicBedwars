4f3b0d1d7046512f150a1cc9a0e1fc65df56c05c
Webhead1104 <webhead1104@hotmail.com>
2025-04-03 17:26:10 -0600

    Block bounds

--- src/main/java/net/cytonic/cytonicbedwars/listeners/BlockPlaceListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/BlockPlaceListener.java
@@ -1,7 +1,11 @@
 package net.cytonic.cytonicbedwars.listeners;
 
 import lombok.NoArgsConstructor;
+import net.cytonic.cytonicbedwars.CytonicBedwarsSettings;
+import net.cytonic.cytosis.utils.Msg;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.GameMode;
 import net.minestom.server.event.player.PlayerBlockPlaceEvent;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.ItemComponent;
@@ -12,14 +16,25 @@ import java.util.Objects;
 public class BlockPlaceListener {
 
     public void onBlockPlace(PlayerBlockPlaceEvent e) {
-        //todo: implement bounds
-        String id = Objects.requireNonNull(e.getPlayer().getItemInHand(e.getHand()).get(ItemComponent.CUSTOM_DATA)).nbt().getString("bwID");
-        if (e.getBlock().hasNbt()) {
-            Block block = e.getBlock().withNbt(Objects.requireNonNull(e.getBlock().nbt()).putBoolean("placedByPlayer", true).putString("bwID", id));
-            e.setBlock(block);
-        } else {
-            Block block = e.getBlock().withNbt(CompoundBinaryTag.builder().putBoolean("placedByPlayer", true).putString("bwID", id).build());
-            e.setBlock(block);
+        if (e.getPlayer().getItemInHand(e.getHand()).has(ItemComponent.CUSTOM_DATA)) {
+            String id = Objects.requireNonNull(e.getPlayer().getItemInHand(e.getHand()).get(ItemComponent.CUSTOM_DATA)).nbt().getString("bwID");
+            if (e.getBlock().hasNbt()) {
+                Block block = e.getBlock().withNbt(Objects.requireNonNull(e.getBlock().nbt()).putBoolean("placedByPlayer", true).putString("bwID", id));
+                e.setBlock(block);
+            } else {
+                Block block = e.getBlock().withNbt(CompoundBinaryTag.builder().putBoolean("placedByPlayer", true).putString("bwID", id).build());
+                e.setBlock(block);
+            }
         }
+        if (e.getPlayer().getGameMode() == GameMode.CREATIVE) return;
+        Pos spawn = CytonicBedwarsSettings.spawnPlatformCenter;
+        if (distance(e.getBlockPosition().x(), spawn.x(), e.getBlockPosition().z(), spawn.z()) > 105.0) {
+            e.setCancelled(true);
+            e.getPlayer().sendMessage(Msg.whoops("You cannot place blocks that far from the map!"));
+        }
+    }
+
+    private double distance(double x1, double x2, double z1, double z2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(z1 - z2, 2));
     }
 }
