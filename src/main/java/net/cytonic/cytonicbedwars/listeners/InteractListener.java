e944183602834ad89f819d98365ca1d8a7c520c2
webhead1104 <webhead1104@hotmail.com>
2025-06-03 16:29:56 -0500

    It will never be null

--- src/main/java/net/cytonic/cytonicbedwars/listeners/InteractListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/InteractListener.java
@@ -4,17 +4,19 @@ import lombok.NoArgsConstructor;
 import net.cytonic.cytonicbedwars.CytonicBedWars;
 import net.cytonic.cytonicbedwars.utils.Items;
 import net.cytonic.cytosis.player.CytosisPlayer;
+import net.minestom.server.component.DataComponents;
 import net.minestom.server.event.player.PlayerBlockInteractEvent;
 import net.minestom.server.event.player.PlayerEntityInteractEvent;
-import net.minestom.server.component.DataComponents;
+
+import java.util.Objects;
 
 @NoArgsConstructor
 public class InteractListener {
 
     public static void onInteract(PlayerBlockInteractEvent event) {
         if (event.getPlayer().getItemInHand(event.getHand()).has(DataComponents.CUSTOM_DATA)) {
-            if (event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA).nbt().getString(Items.NAMESPACE).equals(Items.NAMESPACE)) {
-                String key = event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA).nbt().getString(Items.NAMESPACE);
+            if (Objects.requireNonNull(event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA)).nbt().getString(Items.NAMESPACE).equals(Items.NAMESPACE)) {
+                String key = Objects.requireNonNull(event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA)).nbt().getString(Items.NAMESPACE);
                 CytonicBedWars.getGameManager().getItemAbilityDispatcher().dispatch(key, (CytosisPlayer) event.getPlayer(), event);
             }
         }
@@ -22,8 +24,8 @@ public class InteractListener {
 
     public static void onInteract(PlayerEntityInteractEvent event) {
         if (event.getPlayer().getItemInHand(event.getHand()).has(DataComponents.CUSTOM_DATA)) {
-            if (event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA).nbt().getString(Items.NAMESPACE).equals(Items.NAMESPACE)) {
-                String key = event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA).nbt().getString(Items.NAMESPACE);
+            if (Objects.requireNonNull(event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA)).nbt().getString(Items.NAMESPACE).equals(Items.NAMESPACE)) {
+                String key = Objects.requireNonNull(event.getPlayer().getItemInHand(event.getHand()).get(DataComponents.CUSTOM_DATA)).nbt().getString(Items.NAMESPACE);
                 CytonicBedWars.getGameManager().getItemAbilityDispatcher().dispatch(key, (CytosisPlayer) event.getPlayer(), event);
             }
         }
