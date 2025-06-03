f4b42a757ec96ce910de4ef9f3a290292295d272
webhead1104 <webhead1104@hotmail.com>
2025-06-03 15:27:06 -0500

    Cleanup the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.0.0-beta15"
+    id("io.freefair.lombok") version "8.13.1"
 }
 
 group = "net.cyonic"
@@ -16,21 +15,20 @@ repositories {
 
 dependencies {
     compileOnly("net.cytonic:Cytosis:1.0-SNAPSHOT")
-    compileOnly("org.projectlombok:lombok:1.18.36") // lombok
-    annotationProcessor("org.projectlombok:lombok:1.18.36") // lombok
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
     assemble {
-        dependsOn("shadowJar")
+        dependsOn(shadowJar)
     }
-    named<ShadowJar>("shadowJar") {
-        manifest {
-            attributes["Main-Class"] = "net.cytonic.cytosis.Cytosis"
-        }
-        mergeServiceFiles()
+    shadowJar {
         archiveFileName.set("CytonicBedwars.jar")
         archiveClassifier.set("")
+        mergeServiceFiles()
         if (providers.gradleProperty("server_dir").isPresent) {
             destinationDirectory.set(file(providers.gradleProperty("server_dir").get() + "/plugins"))
         }
