2ec320c275c030a7a18f0dde37c12cee8946e907
webhead1104 <webhead1104@hotmail.com>
2025-06-03 18:57:36 -0500

    Fix

--- src/main/java/net/cytonic/cytonicbedwars/CytonicBedWars.java
+++ src/main/java/net/cytonic/cytonicbedwars/CytonicBedWars.java
@@ -34,7 +34,7 @@ public final class CytonicBedWars implements CytosisPlugin {
                 Logger.error("error", throwable);
                 return;
             }
-            CytonicBedwarsSettings.importConfig(extraData);
+            CytonicBedwarsSettings.importConfig(Cytosis.GSON.toJson(extraData));//todo make getExtraData return a string
             Logger.info("Loading game manager");
             gameManager = new GameManager();
             gameManager.setup();
