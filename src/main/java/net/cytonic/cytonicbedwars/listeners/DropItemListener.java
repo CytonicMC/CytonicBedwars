b3085f29c3bb55230f98ca2ee2a17551dbbe1c33
webhead1104 <webhead1104@hotmail.com>
2025-06-05 10:12:11 -0500

    Prevent players from dropping items while falling

--- src/main/java/net/cytonic/cytonicbedwars/listeners/DropItemListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/DropItemListener.java
@@ -1,5 +1,6 @@
 package net.cytonic.cytonicbedwars.listeners;
 
+import io.github.togar2.pvp.feature.fall.VanillaFallFeature;
 import lombok.NoArgsConstructor;
 import net.cytonic.cytonicbedwars.utils.Items;
 import net.cytonic.cytosis.events.api.Listener;
@@ -23,11 +24,8 @@ public class DropItemListener {
                 event.setCancelled(true);
             }
         }
+        if (event.getPlayer().getTag(VanillaFallFeature.FALL_DISTANCE) > 1) {
+            event.setCancelled(true);
+        }
     }
-
-//todo prevent dropping items while falling
-//        if(player.getFallDistance() > 0){
-//            // prevent dropping items whilst falling
-//            event.setCancelled(true);
-//        }
 }
