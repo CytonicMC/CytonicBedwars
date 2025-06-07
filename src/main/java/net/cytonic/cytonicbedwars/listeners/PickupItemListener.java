e7070a06bfe41e89693b991b16aca7e4c669e35e
webhead1104 <webhead1104@hotmail.com>
2025-06-07 13:43:11 -0500

    Closes #38

--- src/main/java/net/cytonic/cytonicbedwars/listeners/PickupItemListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/PickupItemListener.java
@@ -2,15 +2,24 @@ package net.cytonic.cytonicbedwars.listeners;
 
 import net.cytonic.cytonicbedwars.CytonicBedWars;
 import net.cytonic.cytosis.events.api.Listener;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.Player;
 import net.minestom.server.event.item.PickupItemEvent;
+import net.minestom.server.item.ItemStack;
 
 @SuppressWarnings("unused")
 public class PickupItemListener {
 
     @Listener
     public static void onPickup(PickupItemEvent event) {
-        if (CytonicBedWars.getGameManager().spectators.contains(event.getEntity().getUuid())) {
-            event.setCancelled(true);
+        final Entity entity = event.getLivingEntity();
+        if (entity instanceof Player player) {
+            if (CytonicBedWars.getGameManager().spectators.contains(player.getUuid())) {
+                event.setCancelled(true);
+                return;
+            }
+            final ItemStack itemStack = event.getItemEntity().getItemStack();
+            event.setCancelled(!player.getInventory().addItemStack(itemStack));
         }
     }
 }
