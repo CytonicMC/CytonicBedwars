75ebafd58ce8dca97e58a7a7773e9f76fcd1758e
webhead1104 <webhead1104@hotmail.com>
2025-06-08 13:21:17 -0500

    Fix player list closes #40

--- src/main/java/net/cytonic/cytonicbedwars/data/objects/PlayerList.java
+++ src/main/java/net/cytonic/cytonicbedwars/data/objects/PlayerList.java
@@ -3,13 +3,11 @@ package net.cytonic.cytonicbedwars.data.objects;
 import lombok.NoArgsConstructor;
 import net.cytonic.cytonicbedwars.CytonicBedWars;
 import net.cytonic.cytosis.Cytosis;
-import net.cytonic.cytosis.data.enums.PlayerRank;
 import net.cytonic.cytosis.player.CytosisPlayer;
 import net.cytonic.cytosis.playerlist.Column;
 import net.cytonic.cytosis.playerlist.PlayerListEntry;
 import net.cytonic.cytosis.playerlist.PlayerListFavicon;
 import net.cytonic.cytosis.playerlist.PlayerlistCreator;
-import net.cytonic.cytosis.utils.CytosisPreferences;
 import net.cytonic.cytosis.utils.Msg;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -25,38 +23,78 @@ public class PlayerList implements PlayerlistCreator {
 
     @Override
     public List<Column> createColumns(CytosisPlayer player) {
-        List<Column> columns = new ArrayList<>();
-
+        if (!CytonicBedWars.getGameManager().STARTED) {
+            return List.of(PlayerList.PLAYER_COLUMN.apply(player));
+        }
         List<PlayerListEntry> players = new ArrayList<>();
-
         for (CytosisPlayer p : Cytosis.getOnlinePlayers()) {
-            Team team = !CytonicBedWars.getGameManager().getPlayerTeams().isEmpty() ? CytonicBedWars.getGameManager().getPlayerTeam(p.getUuid()).orElse(null) : null;
-            if (p.getPreference(CytosisPreferences.VANISHED)) {
+            if (CytonicBedWars.getGameManager().getPlayerTeam(p.getUuid()).isEmpty() && (CytonicBedWars.getGameManager().spectators.contains(player.getUuid()) || player.isStaff())) {
+                players.add(new PlayerListEntry(p.getRank().getPrefix().color(NamedTextColor.GRAY).append(p.getName()), p.getRank().ordinal(),
+                        new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getSkin()).textures(), p.getSkin().signature())));
+                continue;
+            }
+            if (p.isVanished()) {
                 if (!player.isStaff()) continue;
                 players.add(new PlayerListEntry(p.getRank().getPrefix().color(NamedTextColor.GRAY)
                         .decorate(TextDecoration.STRIKETHROUGH, TextDecoration.ITALIC).append(p.getName()), p.getRank().ordinal(),
                         new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getSkin()).textures(), p.getSkin().signature())));
                 continue;
             }
-            PlayerRank rank = Cytosis.getRankManager().getPlayerRank(p.getUuid()).orElse(PlayerRank.DEFAULT);
-            players.add(new PlayerListEntry(team == null ? rank.getPrefix().append(p.getName()) : Msg.mm("%s<reset>%s", team.prefix(), p.getUsername()),
-                    rank.ordinal(),
-                    new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getSkin()).textures(), p.getSkin().signature())))
-            ;
+            Team team = CytonicBedWars.getGameManager().getPlayerTeam(p.getUuid()).orElseThrow();
+            if (p.isNicked()) {
+                if (player.getUuid().equals(p.getUuid())) {
+                    players.add(new PlayerListEntry(Msg.mm("%s%s", team.prefix(), p.getTrueUsername()),
+                            p.getTrueRank().ordinal(),
+                            new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getTrueSkin()).textures(), p.getTrueSkin().signature())));
+                    continue;
+                }
+
+                if (player.isStaff()) {
+                    players.add(new PlayerListEntry(Msg.mm("%s<color:#BEFEFE><st><i>%s", team.prefix(), p.getUsername()),
+                            p.getRank().ordinal(),
+                            new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getTrueSkin()).textures(), p.getTrueSkin().signature())));
+                    continue;
+                }
+            }
+            players.add(new PlayerListEntry(Msg.mm("%s%s", team.prefix(), p.getUsername()),
+                    p.getRank().ordinal(),
+                    new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getSkin()).textures(), p.getSkin().signature())));
         }
 
-        Column playerCol = new Column(Msg.mm("<white><b>        Players    "), PlayerListFavicon.WHITE);
-        if (players.size() >= 19) {
-            int extra = players.size() - 19;
-            players = new ArrayList<>(players.subList(0, 18));
-            players.add(new PlayerListEntry(Msg.mm("<italic> + " + extra + " more"), 100));
-        } else {
-            playerCol.setEntries(new ArrayList<>(players));
-            players.clear();
-        }
-        columns.add(playerCol);
-        return columns;
+        Column playerCol = new Column(Msg.mm("<dark_purple><b>        Players    "), PlayerListFavicon.PURPLE);
+        playerCol.setEntries(new ArrayList<>(players));
+        return List.of(playerCol);
     }
+
+
+    //List<Column> columns = new ArrayList<>();
+    //
+    //        List<PlayerListEntry> players = new ArrayList<>();
+    //
+    //        for (CytosisPlayer p : Cytosis.getOnlinePlayers()) {
+    //            Team team = !CytonicBedWars.getGameManager().getPlayerTeams().isEmpty() ? CytonicBedWars.getGameManager().getPlayerTeam(p.getUuid()).orElse(null) : null;
+    //            if (p.getPreference(CytosisPreferences.VANISHED)) {
+    //                if (!player.isStaff()) continue;
+    //                players.add(new PlayerListEntry(p.getRank().getPrefix().color(NamedTextColor.GRAY)
+    //                        .decorate(TextDecoration.STRIKETHROUGH, TextDecoration.ITALIC).append(p.getName()), p.getRank().ordinal(),
+    //                        new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getSkin()).textures(), p.getSkin().signature())));
+    //                continue;
+    //            }
+    //            PlayerRank rank = Cytosis.getRankManager().getPlayerRank(p.getUuid()).orElse(PlayerRank.DEFAULT);
+    //            players.add(new PlayerListEntry(team == null ? rank.getPrefix().append(p.getName()) : Msg.mm("%s<reset>%s", team.prefix(), p.getUsername()),
+    //                    rank.ordinal(),
+    //                    new PlayerInfoUpdatePacket.Property("textures", Objects.requireNonNull(p.getSkin()).textures(), p.getSkin().signature())))
+    //            ;
+    //        }
+    //
+    //        Column playerCol = new Column(Msg.mm("<white><b>        Players    "), PlayerListFavicon.WHITE);
+    //        if (players.size() >= 19) {
+    //            int extra = players.size() - 19;
+    //            players = new ArrayList<>(players.subList(0, 18));
+    //            players.add(new PlayerListEntry(Msg.mm("<italic> + " + extra + " more"), 100));
+    //        } else {
+    //            playerCol.setEntries(new ArrayList<>(players));
+    //            players.clear();
 
     /**
      * Creates the header for the player
