a40d247de210975e313cf0b28bbc8152f3ffa23c
webhead1104 <webhead1104@hotmail.com>
2025-06-29 13:56:09 -0500

    Add support for dropping items closes #60

--- src/main/java/net/cytonic/cytonicbedwars/listeners/DropItemListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/DropItemListener.java
@@ -3,12 +3,15 @@ package net.cytonic.cytonicbedwars.listeners;
 import io.github.togar2.pvp.feature.fall.VanillaFallFeature;
 import lombok.NoArgsConstructor;
 import net.cytonic.cytonicbedwars.utils.Items;
+import net.cytonic.cytosis.Cytosis;
 import net.cytonic.cytosis.events.api.Listener;
-import net.cytonic.cytosis.events.api.Priority;
 import net.minestom.server.component.DataComponents;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.event.item.ItemDropEvent;
 import net.minestom.server.item.ItemStack;
 
+import java.time.Duration;
 import java.util.Objects;
 
 @NoArgsConstructor
@@ -16,7 +19,6 @@ import java.util.Objects;
 public class DropItemListener {
 
     @Listener
-    @Priority(-1)
     public void onDrop(ItemDropEvent event) {
         ItemStack item = event.getItemStack();
         if (item.has(DataComponents.CUSTOM_DATA)) {
@@ -26,6 +28,14 @@ public class DropItemListener {
         }
         if (event.getPlayer().getTag(VanillaFallFeature.FALL_DISTANCE) > 1) {
             event.setCancelled(true);
+        }
+
+        if (!event.isCancelled()) {
+            Pos playerPos = event.getPlayer().getPosition();
+            ItemEntity itemEntity = new ItemEntity(event.getItemStack());
+            itemEntity.setPickupDelay(Duration.ofMillis(500));
+            itemEntity.setInstance(Cytosis.getDefaultInstance(), playerPos.add(0, 1.5, 0));
+            itemEntity.setVelocity(playerPos.direction().mul(6));
         }
     }
 }
