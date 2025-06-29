e91e75919b13b22d359d1be231161c1a5a9601cc
webhead1104 <webhead1104@hotmail.com>
2025-06-29 17:43:28 -0500

    Use the minestom Tag system

--- src/main/java/net/cytonic/cytonicbedwars/listeners/UseItemListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/UseItemListener.java
@@ -5,11 +5,8 @@ import net.cytonic.cytonicbedwars.CytonicBedWars;
 import net.cytonic.cytonicbedwars.player.BedwarsPlayer;
 import net.cytonic.cytonicbedwars.utils.Items;
 import net.cytonic.cytosis.events.api.Listener;
-import net.minestom.server.component.DataComponents;
 import net.minestom.server.event.player.PlayerUseItemEvent;
 import net.minestom.server.item.ItemStack;
-
-import java.util.Objects;
 
 @NoArgsConstructor
 @SuppressWarnings("unused")
@@ -19,11 +16,9 @@ public class UseItemListener {
     public void onInteract(PlayerUseItemEvent event) {
         if (!(event.getPlayer() instanceof BedwarsPlayer player)) return;
         ItemStack item = event.getPlayer().getItemInHand(event.getHand());
-        if (item.has(DataComponents.CUSTOM_DATA)) {
-            if (Objects.requireNonNull(item.get(DataComponents.CUSTOM_DATA)).nbt().get(Items.NAMESPACE) != null) {
-                String key = Objects.requireNonNull(item.get(DataComponents.CUSTOM_DATA)).nbt().getString(Items.NAMESPACE);
-                CytonicBedWars.getGameManager().getItemAbilityDispatcher().dispatch(key, player, event);
-            }
+        if (item.hasTag(Items.NAMESPACE)) {
+            String key = item.getTag(Items.NAMESPACE);
+            CytonicBedWars.getGameManager().getItemAbilityDispatcher().dispatch(key, player, event);
         }
     }
 }
