9132d0b7dfb48c3bd5a52359cfbe99df53ac5290
webhead1104 <webhead1104@hotmail.com>
2025-06-30 10:55:14 -0500

    Sword stuff closes #55

--- src/main/java/net/cytonic/cytonicbedwars/listeners/DropItemListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/DropItemListener.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import net.cytonic.cytonicbedwars.utils.Items;
 import net.cytonic.cytosis.Cytosis;
 import net.cytonic.cytosis.events.api.Listener;
+import net.cytonic.cytosis.logging.Logger;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.event.item.ItemDropEvent;
@@ -22,10 +24,25 @@ public class DropItemListener {
         if (item.hasTag(Items.NO_DROP)) {
             if (item.getTag(Items.NO_DROP)) {
                 event.setCancelled(true);
+                return;
             }
         }
         if (event.getPlayer().getTag(VanillaFallFeature.FALL_DISTANCE) > 1) {
             event.setCancelled(true);
+            return;
+        }
+
+        if (item.hasTag(Items.NAMESPACE) && item.getTag(Items.NAMESPACE).contains("SWORD") && !item.getTag(Items.NAMESPACE).contains("MENU")) {
+            int swords = -1; //the sword in the event is still in the inventory
+            for (ItemStack itemStack : event.getPlayer().getInventory().getItemStacks()) {
+                if (itemStack.hasTag(Items.NAMESPACE) && itemStack.getTag(Items.NAMESPACE).contains("SWORD") && !itemStack.getTag(Items.NAMESPACE).contains("MENU")) {
+                    swords++;
+                }
+            }
+            Logger.debug("you have %s swords", swords);
+            if (swords == 0) {
+                MinecraftServer.getSchedulerManager().buildTask(() -> event.getPlayer().getInventory().addItemStack(Items.DEFAULT_SWORD)).delay(Duration.ofMillis(1)).schedule();
+            }
         }
 
         if (!event.isCancelled()) {
--- src/main/java/net/cytonic/cytonicbedwars/listeners/PickupItemListener.java
+++ src/main/java/net/cytonic/cytonicbedwars/listeners/PickupItemListener.java
@@ -1,7 +1,9 @@
 package net.cytonic.cytonicbedwars.listeners;
 
 import net.cytonic.cytonicbedwars.CytonicBedWars;
+import net.cytonic.cytonicbedwars.utils.Items;
 import net.cytonic.cytosis.events.api.Listener;
+import net.cytonic.cytosis.logging.Logger;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.item.PickupItemEvent;
@@ -15,6 +17,14 @@ public class PickupItemListener {
         final Entity entity = event.getLivingEntity();
         if (entity instanceof Player player) {
             if (CytonicBedWars.getGameManager().getSpectators().contains(player.getUuid())) {
+                event.setCancelled(true);
+            }
+            final ItemStack item = event.getItemEntity().getItemStack();
+            Logger.debug("item name = %s namespace = %s", item.material().name(), item.getTag(Items.NAMESPACE));
+            if (item.hasTag(Items.NAMESPACE) && item.getTag(Items.NAMESPACE).contains("SWORD") && !item.getTag(Items.NAMESPACE).contains("MENU")) {
+                CytonicBedWars.getGameManager().getPlayerInventoryManager().setSword(item, player);
+                Logger.debug("set sword");
+                event.getItemEntity().setItemStack(ItemStack.AIR);
                 event.setCancelled(true);
                 return;
             }
