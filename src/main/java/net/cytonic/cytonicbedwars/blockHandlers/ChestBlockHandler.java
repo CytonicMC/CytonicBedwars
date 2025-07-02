a0417ffbc93a55daee87af0f41fa36b4ec4df40c
webhead1104 <webhead1104@hotmail.com>
2025-07-02 13:26:49 -0500

    Change the sounds and make them play at the block

--- src/main/java/net/cytonic/cytonicbedwars/blockHandlers/ChestBlockHandler.java
+++ src/main/java/net/cytonic/cytonicbedwars/blockHandlers/ChestBlockHandler.java
@@ -4,14 +4,19 @@ import net.cytonic.cytonicbedwars.CytonicBedWars;
 import net.cytonic.cytonicbedwars.data.objects.Team;
 import net.cytonic.cytonicbedwars.player.BedwarsPlayer;
 import net.cytonic.cytonicbedwars.utils.Items;
+import net.cytonic.cytosis.Cytosis;
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.event.inventory.InventoryCloseEvent;
 import net.minestom.server.event.inventory.InventoryPreClickEvent;
 import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.network.packet.server.play.BlockActionPacket;
+import net.minestom.server.sound.SoundEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
 
 public class ChestBlockHandler implements BlockHandler {
     @Override
@@ -27,11 +32,15 @@ public class ChestBlockHandler implements BlockHandler {
                 event.setCancelled(true);
             }
         });
-        team.getTeamChest().eventNode().addListener(EventListener.of(InventoryCloseEvent.class, event -> event.getPlayer().sendPacket(new BlockActionPacket(interaction.getBlockPosition(), (byte) 1, (byte) 0, interaction.getBlock()))));
+        team.getTeamChest().eventNode().addListener(EventListener.of(InventoryCloseEvent.class, event -> {
+            Cytosis.getDefaultInstance().sendGroupedPacket(new BlockActionPacket(interaction.getBlockPosition(), (byte) 1, (byte) 0, interaction.getBlock()));
+            Cytosis.getDefaultInstance().playSound(Sound.sound(SoundEvent.BLOCK_CHEST_CLOSE, Sound.Source.MASTER, 0.5f, new Random().nextFloat() * 0.1F + 0.9F), interaction.getBlockPosition());
+        }));
         player.eventNode().addListener(listener);
         player.openInventory(team.getTeamChest());
         team.getTeamChest().addViewer(player);
-        player.sendPacket(new BlockActionPacket(interaction.getBlockPosition(), (byte) 1, (byte) 1, interaction.getBlock()));
+        Cytosis.getDefaultInstance().playSound(Sound.sound(SoundEvent.BLOCK_CHEST_OPEN, Sound.Source.MASTER, 0.5f, new Random().nextFloat() * 0.1F + 0.9F), interaction.getBlockPosition());
+        Cytosis.getDefaultInstance().sendGroupedPacket(new BlockActionPacket(interaction.getBlockPosition(), (byte) 1, (byte) 1, interaction.getBlock()));
         return true;
     }
 
